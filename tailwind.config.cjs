a6b7c15e56dc1e7fcdc65373f083556054aa1a3d
Deece94 <Dcollar@hotmail.com.au>
2022-11-19 12:08:28 +1100

    index page design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {
-            colors: {
-                "dark-blue": "#090D46",
-            },
+        extend: {},
+        colors: {
+            "dark-blue": "#090D46",
+            "light-red": "#ff3c3c",
+            "dark-red": "#d91f1f",
         },
     },
     plugins: [],
