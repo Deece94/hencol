82b2c648efc548b5b08643565c06ae05aeb92ded
Deece94 <Dcollar@hotmail.com.au>
2022-11-19 12:13:14 +1100

    fixing tailwind config for hosting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
-        colors: {
-            "dark-blue": "#090D46",
-            "light-red": "#ff3c3c",
-            "dark-red": "#d91f1f",
+        extend: {
+            colors: {
+                "dark-blue": "#090D46",
+                "light-red": "#ff3c3c",
+                "dark-red": "#d91f1f",
+            },
         },
     },
     plugins: [],
